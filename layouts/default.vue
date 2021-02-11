1701e49328198d6799d36076b141b22298d559f7
Mario Hotaj <43809652+PowerShot@users.noreply.github.com>
2021-02-11 11:07:17 +0100

    2 pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,6 @@
   <div>
     <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <div class="navbar-item">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </div>
 
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
@@ -39,6 +36,18 @@
           </div>
         </div>
     </div>
+    <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary"  @click="alertJeuSerieu">
+                <strong>Qu'es-ce qu'un jeu sérieux ?</strong>
+              </a>
+              <a class="button is-light" href="/fiche">
+                Aide à domicile (fiche métier)
+              </a>
+            </div>
+          </div>
+        </div>
   </div>
 </nav>
     <!-- <nav class="navbar is-primary">
@@ -84,6 +93,15 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    alertJeuSerieu() {
+      this.$buefy.dialog.alert({
+          title: "Qu'es-ce qu'un jeu sérieux ?",
+          message: "Un <b>jeu sérieux</b> est une activité pédagogique avec des ressorts de divertissement, associant un objectif sérieux.<br> C’est une application informatique, dont l'objectif est de combiner à la fois des aspects sérieux tels, de manière non exhaustive, l'enseignement, l'apprentissage, la communication, ou encore l'information, avec des ressorts ludiques issus du jeu vidéo.<br> Une telle association a donc pour but de s'écarter du simple divertissement.<br><br><a href='http://www.cndp.fr/crdp-reims/index.php?id=2237' target='_blank'>En savoir plus</a>",
+          confirmText: 'OK'
+      })
+    },
   }
 }
 </script>
---
+++ pages/fiche.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="section">
+    <h1 class="title is-1">Fiche métier Aide à domicile</h1>
+    <p>L'aide à domicile assiste les personnes fragiles, dépendantes ou en difficulté sociale ainsi que les familles en les aidant dans l'accomplissement des tâches et des activités de la vie quotidienne, et en fournissant un soutien psychologique et social.
+    Il accompagne les personnes sur leur lieu de vie dans l'accomplissement d'activités courantes (ex. entretien de la maison, tâches administratives quotidiennes) et aide les personnes ne pouvant plus faire en totale autonomie et/ou rencontrant des difficultés passagères ou durables. </p>
+    <br/>
+    <h1 class="title">Activités actuelles</h1>
+
+    Accompagner une personne dans la réalisation de ses activités quotidiennes (aider à la toilette, à l'habillage, au déshabillage…) et au niveau des tâches domestique (entretien du logement, du linge, courses, préparation des repas, démarches administratives…).
+    Assister une personne pour les actes qui la concernent personnellement (hygiène corporelle, alimentation, aide aux déplacements…).
+    Accompagner et aider les personnes dans les activités de la vie sociale et relationnelle (stimuler les relations sociales, accompagner dans les activités de loisirs...).
+    Garder des enfants en bas âge, en prenant en charge les aspects matériels (lever, coucher, toilette, habillage, confection et prise des repas) ainsi que la réalisation d'activités éducatives, de socialisation et d'éveil (jeux, promenades, devoirs, lectures, etc.).
+    Participer à l'évaluation de la situation et adapter son intervention en conséquence.<br/><br/>
+
+    <h1 class="title">Conditions d'exercice</h1>
+    L'aide à domicile exerce en service d'aide à domicile, au domicile des usagers.<br/><br/>
+
+    <h1 class="title">Accès au métier</h1>
+    Principales formations Niveau V :<br/>
+    BEP carrières sanitaires et sociales,<br/>
+    Mention complémentaire Aide à domicile,<br/>
+    Diplôme d'Etat d'Auxiliaire de Vie sociale (DE AVS),<br/>
+    Titre assistant de vie aux familles (ADVF),<br/>
+    CAPA option services en milieu rural,<br/>
+    BEPA service, option services aux personnes.<br/>
+    <br/>
+    Niveau IV :<br/>
+    Bac pro ASSP (Accompagnement Soins et Service à la Personne, option « A domicile »).<br/><br/>
+
+
+    <h1 class="title">Particularités du métier dans les mutuelles</h1>
+    Les services à domicile sont fréquemment adossés à une large palette de services : soins de santé fournis par des SSIAD (aides-soignants et infirmiers) ou des centres de santé, services de portage de repas, services de réparation ou d'entretien, de jardinage, services culturels etc. Cela offre des perspectives d'évolution professionnelle plus importante pour les aides à domicile, au sein des différents services.
+    <br/><br/>
+    Les aides à domicile contribuent fréquemment à la socialisation des personnes aidées ou accompagnées.
+    <br/><br/>
+    Les mutuelles veillent particulièrement au développement de la professionnalisation et de la montée en qualification des intervenants.
+
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+  components: {
+    Card
+  }
+}
+</script>
